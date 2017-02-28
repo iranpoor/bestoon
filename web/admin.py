bb2447099c0b9d354781039f160aed1c1c360aef
iranpoor <saeedtk2005@gmail.com>
2017-02-28 16:40:53 +0330

    web module

---
+++ web/admin.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from django.contrib import admin
+from .models import Expense ,income, Token
+
+
+
+
+# Register your models here.
+admin.site.register(Expense)
+admin.site.register(income)
+admin.site.register(Token)
+
+
---
+++ web/models.py
@@ -0,0 +1,34 @@
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Token(models.Model):
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    Token = models.CharField(max_length=48)
+
+    def __str__(self):
+        return "{}_Token".format(self.User)
+
+
+# Create your models here.
+class Expense(models.Model):
+    print('expense')
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        print('unicode')
+        return "{}-{}".format(self.date, self.amount)
+
+
+
+class income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+    def  __str__ (self):
+        return  "{}-{}" .format(self.date, self.amount)
